8df2e72c42f22c196f4f372a9c4efdc34850f2cd
DevLeeYG <hi33741@naver.com>
2022-04-22 15:09:16 +0900

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import styled from "styled-components"
+import Header from '../components/Header';
+import palette from '../styles/palette';
 
 const Container = styled.div`
     font-size : 21px;
     color:gray;
+    width:100%;
+    background:'black';
+  
+  
+
+`
+const Aa = styled.div`
+background-color:${palette.black}
+    width:100%;
+    height:300px;
 `
 
-
-const index:React.FC = () => {
+const index = () => {
     return (
-        <div>
-            hello next
-        </div>
+        <Container>
+         <Header/>
+        </Container>
     );
 };
 
