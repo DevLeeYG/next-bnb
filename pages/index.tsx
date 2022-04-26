5759fbe7318a2df3ac78a0c4fb96bf4b5554ea7e
DevLeeYG <hi33741@naver.com>
2022-04-26 10:49:09 +0900

    Revert "1"
    This reverts commit 8df2e72c42f22c196f4f372a9c4efdc34850f2cd.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import styled from "styled-components"
-import Header from '../components/Header';
-import palette from '../styles/palette';
 
 const Container = styled.div`
     font-size : 21px;
     color:gray;
-    width:100%;
-    background:'black';
-  
-  
-
-`
-const Aa = styled.div`
-background-color:${palette.black}
-    width:100%;
-    height:300px;
 `
 
-const index = () => {
+
+const index:React.FC = () => {
     return (
-        <Container>
-         <Header/>
-        </Container>
+        <div>
+            hello next
+        </div>
     );
 };
 
