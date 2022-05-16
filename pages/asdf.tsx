0bae1b2ace3d99031f7fd14a1627ed4cf65769c0
DevLeeYG <hi33741@naver.com>
2022-05-16 16:15:01 +0900

    [1번커밋]1
    1

--- pages/asdf.tsx
+++ pages/asdf.tsx
@@ -1,6 +1,10 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
 
-const Asdf = () => <div />;
+const Asdf = () => (
+  <div>
+    <span>1</span>
+  </div>
+);
 
 export default Asdf;
