f07c039cb78f5061a0a69370522284a78218a11b
DevLeeYG <hi33741@naver.com>
2022-05-16 15:48:50 +0900

    [next] js1
    123

--- pages/asdf.tsx
+++ pages/asdf.tsx
@@ -1,8 +1,10 @@
+// eslint-disable-next-line no-use-before-define
 import React from 'react';
 
 const Asdf = () => (
   <div>
     asdfasdf
+    <span>asdfasdf</span>
     <span>asdfasdf</span>
   </div>
 );
